001335d33f457ad634f2fcc0b14a1a366b04bf3c
Edwin <edwingenova@gmail.com>
2020-04-07 21:28:45 -0400

    added some styling and worked on intro section

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,6 +11,7 @@ export default function Intro(){
         }} className="hero column col-11 col-xs-12 container">
             <div className="hero-body">
                 <h1>Welcome!</h1>
+                <p>This is my portfolio, I created it mainly with React, Node.js and the Spectre.css framework. Here you'll be able to see some projects that I'm proud of (deployed link's and repo's), check out my resume and learn about it me. Hope you enjoy, thanks for visiting!</p>
             </div>
         </div>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ export default function Navbar(){
     return(
 <header className="navbar container">
   <section className="navbar-section">
-    <a href="#intro" className="navbar-brand mr-2">Intro</a>
+    <a href="#intro" className="btn btn-link">Intro</a>
     <a href="#projects" className="btn btn-link">Projects</a>
-    <a href="#about" className="btn btn-link">GitHub</a>
+    <a href="#about" className="btn btn-link">About</a>
   </section>
 </header>
     );
