0ba7f8e262b0df633302e63c987f2e304027e1ee
Edwin <edwingenova@gmail.com>
2020-04-08 18:13:29 -0400

    Updates

---
+++ precache-manifest.9a3fff75879b649c80943a9d833ad1ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec7cba532d86bdd26716c610ff5d578",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "22518afbb5cba2594bb4",
+    "url": "/React-Portfolio/static/css/2.929bb6f5.chunk.css"
+  },
+  {
+    "revision": "12392592d5bbbd56a343",
+    "url": "/React-Portfolio/static/css/main.a314541b.chunk.css"
+  },
+  {
+    "revision": "22518afbb5cba2594bb4",
+    "url": "/React-Portfolio/static/js/2.5a8eff49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.5a8eff49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12392592d5bbbd56a343",
+    "url": "/React-Portfolio/static/js/main.0be5c4e7.chunk.js"
+  },
+  {
+    "revision": "49b6db34b5df1bb9db46",
+    "url": "/React-Portfolio/static/js/runtime-main.4d47df45.js"
+  },
+  {
+    "revision": "cad0027f6e9741f7ef8e56c060e86bce",
+    "url": "/React-Portfolio/static/media/SoftwareResume.cad0027f.pdf"
+  },
+  {
+    "revision": "af9df0529c27110e42183757da882522",
+    "url": "/React-Portfolio/static/media/example.af9df052.jpeg"
+  },
+  {
+    "revision": "925a1dd0e673e419f4e28b8b2f3b980a",
+    "url": "/React-Portfolio/static/media/project1.925a1dd0.jpeg"
+  },
+  {
+    "revision": "642fcfbdee84ccbb03d5fbca6faf9b07",
+    "url": "/React-Portfolio/static/media/project2screenshot.642fcfbd.jpeg"
+  },
+  {
+    "revision": "885c7b0208d06fe3257c2f9dd3ad675d",
+    "url": "/React-Portfolio/static/media/readmescreenshot.885c7b02.jpeg"
+  }
+]);
